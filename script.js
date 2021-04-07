7335fc865bf50e664a8cb3c5c0fe80be060a0e48
sanjay <sanjayjaiswal586@gmail.com>
2021-04-07 17:31:38 +0530

    displaying the other details

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
-const userName=document.querySelector(".input");
+const url= "https://api.github.com/users/"
 
-console.log(userName);
+
+
+  function grabthetxt(){
+
+
+    var clear=document.getElementById("placehere");
+    var clear2=document.getElementById("details");
+    
+
+    clear.innerText="";    
+    var userName=document.querySelector("#input").value;
+
+    var result=url.concat(userName);
+    var translatedtext="";
+    var name=fetch(result)
+    .then(response => response.json())
+    .then(json=>{
+
+   
+    
+
+
+        var translatedtext = json.avatar_url;
+        var elem = document.createElement("img");
+        elem.src = String(translatedtext);
+   
+    elem.setAttribute("alt", "img");
+    clear.appendChild(elem);
+    var spanTag=document.createElement("p");
+    spanTag.classList.add("user-name");
+    var quoteTag=document.createTextNode("Name: "+ json.name);
+   spanTag.appendChild(quoteTag);
+    clear.appendChild(spanTag);
+  
+    var spanTag=document.createElement("span");
+    spanTag.classList.add("user-name");
+    var quoteTag=document.createTextNode("Follwers: "+ json.followers);
+   spanTag.appendChild(quoteTag);
+    clear.appendChild(spanTag);
+  
+
+  
+    // var translatedtext1 = json.name;
+    // console.log(translatedtext1);
+    
+    })
+
+     
+ 
+
+  }
+
+
+
+//   var name=fetch("https://api.github.com/users/sanjay-xdr/followers")
+//     .then(response => response.json())
+//     .then(json=>{
+//        json.forEach( element =>{console.log(element)})
+// });
